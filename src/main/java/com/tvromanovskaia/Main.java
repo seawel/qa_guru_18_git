231bbc10edd67aafbe460416ec63ca84876553fd
Tanya Romanovskaia <tvromanovskaia@gmail.com>
2025-01-23 22:16:12 +0300

    added message method

--- src/main/java/com/tvromanovskaia/Main.java
+++ src/main/java/com/tvromanovskaia/Main.java
@@ -6,8 +6,10 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello QA Guru!");
+        System.out.println ( getMessage ());
 
-
+private static String getMessage() {
+    return "hello QA guru";add
+        }
     }
 }

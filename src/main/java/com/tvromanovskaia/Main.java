2e357aa03a605508eafd99a202dd60c5c62a12dc
seawel <123826463+seawel@users.noreply.github.com>
2025-01-23 22:36:25 +0300

    Update Main.java

--- src/main/java/com/tvromanovskaia/Main.java
+++ src/main/java/com/tvromanovskaia/Main.java
@@ -5,7 +5,7 @@ package com.tvromanovskaia;
 public class Main {
     
     
-    private static String msg = "Hello QA Guru"
+    private static String BFYT = "Hello QA Guru"
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
